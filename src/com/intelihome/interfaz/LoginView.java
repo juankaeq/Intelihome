0e859495225b5adfd936a6757145139150889bbd
juankaeq <juankaeq@hotmail.com>
2013-12-08 20:10:13 -0500

    Cambio en project.property

--- src/com/intelihome/interfaz/LoginView.java
+++ src/com/intelihome/interfaz/LoginView.java
@@ -10,7 +10,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class LoginView extends Activity implements OnClickListener {
 

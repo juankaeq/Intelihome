865e7f206e2b32d13a007ad8e587fe363fb7cfbe
juankaeq <juankaeq@hotmail.com>
2013-12-08 19:00:35 -0500

    Clase control

--- src/com/intelihome/perfiles/Perfil.java
+++ src/com/intelihome/perfiles/Perfil.java
@@ -1,5 +1,49 @@
 package com.intelihome.perfiles;
 
+import java.util.ArrayList;
+
+import com.intelihome.dispositivos.Dispositivo;
+
 public class Perfil {
 
+	private String nombre;
+	private String horaInicio;
+	private String horaFin;
+	
+	private ArrayList<Dispositivo> dispositivosAsociados = new ArrayList<Dispositivo>();
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getHoraInicio() {
+		return horaInicio;
+	}
+
+	public void setHoraInicio(String horaInicio) {
+		this.horaInicio = horaInicio;
+	}
+
+	public String getHoraFin() {
+		return horaFin;
+	}
+
+	public void setHoraFin(String horaFin) {
+		this.horaFin = horaFin;
+	}
+
+	public ArrayList<Dispositivo> getDispositivosAsociados() {
+		return dispositivosAsociados;
+	}
+
+	public void setDispositivosAsociados(
+			ArrayList<Dispositivo> dispositivosAsociados) {
+		this.dispositivosAsociados = dispositivosAsociados;
+	}
+	
+	
 }
---
+++ src/com/intelihome/perfiles/PerfilControl.java
@@ -0,0 +1,28 @@
+package com.intelihome.perfiles;
+
+import java.util.ArrayList;
+
+public class PerfilControl {
+
+	public static ArrayList<Perfil> perfilesTotales = new ArrayList<Perfil>();
+	public static ArrayList<Perfil> perfilesActivos = new ArrayList<Perfil>();
+	
+	
+	public static ArrayList<Perfil> consultarPerfilQuemado(){
+	
+		perfilesActivos = new ArrayList<Perfil>();
+		for (int i = 0; i < 10; i++) {
+			
+			Perfil p = new Perfil();
+			p.setNombre("Perfil"+(i+1));
+			p.setHoraFin(""+(i+1));
+			
+			perfilesActivos.add(p);
+			
+		}
+		
+		return perfilesActivos;
+		
+	}
+	
+}
